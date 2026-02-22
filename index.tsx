870cf86852757a54df869fb3aa07cdfa3eae63cc
patoperez <patg4mer@gmail.com>
2026-02-21 22:18:08 -0700

    fix: add Unhead provider to root to prevent blank page

--- index.tsx
+++ index.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { UnheadProvider, createHead } from "@unhead/react/client";
+
+const head = createHead();
+
 const rootElement = document.getElementById("root");
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
@@ -9,8 +13,10 @@ if (!rootElement) {
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <UnheadProvider head={head}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </UnheadProvider>
   </React.StrictMode>,
 );
